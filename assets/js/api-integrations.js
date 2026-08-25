ac7924756316b7be28975cf8a8c09a835641aa0e
Ubuntu <ubuntu@ip-172-31-34-60.eu-central-1.compute.internal>
2026-08-25 12:30:57 +0000

    api url update

--- assets/js/api-integrations.js
+++ assets/js/api-integrations.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = window.APP_CONFIG?.API_BASE_URL ;
-
+const API_BASE_URL ="https://api.ecosystem.jovianex.com/api/v1";
+        
 async function getMemberCount() {
   try {
     const response = await fetch(`${API_BASE_URL}/users/count`);
